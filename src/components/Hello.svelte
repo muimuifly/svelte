451a701b6caf699d4b165efc4889e796f96770fd
muimuifly <147339997+muimuifly@users.noreply.github.com>
2024-12-16 12:50:22 -0500

    Add files via upload

---
+++ src/components/Hello.svelte
@@ -0,0 +1,9 @@
+<script>
+    let {name} = $props();
+</script>
+
+{#if name === 'world'}
+<h1>Hello {name}!</h1>
+{:else}
+<h1>I don't say hello to anyone else</h1>
+{/if}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<script>
+    let name = $state('Anni');
+</script>
+<input bind:value={name} placeholder= "Enter your name" />
+<p>Hello {name}!</p>
+
